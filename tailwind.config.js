902195f888c8908ccf210986a3bef308f52313a4
SimenHagen86 <jo.simen.hagen@gmail.com>
2023-10-21 22:06:36 +0200

    done with mobile view

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,27 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      ' p-lightred': 'hsl(0, 100%, 67%)',
-      'p-Orangeyyellow': 'hsl(39, 100%, 56%)',
-      'p-Greenteal': 'hsl(166, 100%, 37%)',
-      'p-Cobaltblue': 'hsl(234, 85%, 45%)',
-      'g-Lightslateblue': 'hsl(252, 100%, 67%)',
-      'g-Lightroyalblue': 'hsl(241, 81%, 54%)',
-      'g2-violetblue ': 'hsla(256, 72%, 46%, 1)',
-      'g2-Persianblue': 'hsla(241, 72%, 46%, 0)',
-      ' White': 'hsl(0, 0%, 100%)',
-      Paleblue: 'hsl(221, 100%, 96%)',
+    extend: {
+      colors: {
+        'p-lightred': 'hsl(0, 100%, 67%)',
+        'p-Orangeyyellow': 'hsl(39, 100%, 56%)',
+        'p-Greenteal': 'hsl(166, 100%, 37%)',
+        'p-Cobaltblue': 'hsl(234, 85%, 45%)',
+        'g-Lightslateblue': 'hsl(252, 100%, 67%)',
+        'g-Lightroyalblue': 'hsl(241, 81%, 54%)',
+        'g2-violet-blue ': 'hsla(256, 72%, 46%, 1)',
+        'g2-Persianblue': 'hsla(241, 72%, 46%, .5)',
+        ' White': 'hsl(0, 0%, 100%)',
+        Paleblue: 'hsl(221, 100%, 96%)',
 
-      Lightlavender: 'hsl(241, 100%, 89%)',
+        Lightlavender: 'hsl(241, 100%, 89%)',
 
-      Darkgrayblue: 'hsl(224, 30%, 27%)',
+        Darkgrayblue: 'hsl(224, 30%, 27%)',
+      },
     },
-    extend: {},
+    fontFamily: {
+      hanken: ['Hanken Grotesk', 'sans-serif'],
+    },
   },
   plugins: [],
 }
